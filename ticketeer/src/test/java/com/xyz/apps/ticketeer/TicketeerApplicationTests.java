a92c11e3015964b02d0c14eede7d64cf55567752
J0101 <subhajoy.laskar@comakeit.com>
2022-02-13 16:05:46 +0530

    xyz ticketeer - initial commit.

---
+++ ticketeer/src/test/java/com/xyz/apps/ticketeer/TicketeerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyz.apps.ticketeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
